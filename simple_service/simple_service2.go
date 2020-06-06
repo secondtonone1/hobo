e6c2a8bafcc0291ab5fc1ba95a20d5cb06ce8750
secondtonone1 <secondtonone1@163.com>
2020-06-06 13:30:54 +0800

    modified:   logs/server.log 	deleted:    main.exe 	deleted:    main.exe.pid 	modified:   simple_service/simple_service2.go

--- simple_service/simple_service2.go
+++ simple_service/simple_service2.go
@@ -2,6 +2,7 @@ package simple_service
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/duanhf2012/origin/node"
 	"github.com/duanhf2012/origin/service"
@@ -17,11 +18,12 @@ type Simple_Service2 struct {
 
 func (ss *Simple_Service2) OnInit() error {
 	fmt.Println("simple service2 oninit success")
-
+	ss.AfterFunc(time.Second*1, ss.onSecondTick)
 	return nil
 }
 
 //设置定时器
 func (ss *Simple_Service2) onSecondTick() {
-
+	fmt.Printf("tick.\n")
+	ss.AfterFunc(time.Second*1, ss.onSecondTick)
 }
